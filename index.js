7bbf2cd9cf734e886c96c55b8e6734a9027b9053
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-03-24 09:30:56 +0100

    Initial implementation of the additional actions

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ function getAccountId() {
 		console.log(`Error getting User Guid: ${err}`);
 	});
 }
+
+async function getTwitchUser(id) {
+	return new Promise((resolve, reject) => resolve(id)); // TODO: Mock until implementation
+}
+
 async function say() {
 	let guid = document.getElementById('acc_id').value;
 	let jwtToken = document.getElementById('jwt_token').value;
@@ -38,6 +43,151 @@ async function say() {
 	}).then(res => {
 		if (!res.ok)
 			console.log(`Non-OK-Response (say): ${res.status}`);
+		return res.json();
+	}).then(data => {
+		document.getElementById('result').value = JSON.stringify(data, null, 2);
+	}).catch(err => {
+		console.log(`Error sending Twitch Message: ${err}`);
+	});
+}
+
+async function timeout() {
+	let guid = document.getElementById('acc_id').value;
+	let jwtToken = document.getElementById('jwt_token').value;
+	let user = document.getElementById('timeoutuser').value.toLowerCase();
+	let isId = user.startsWith('id:');
+	if (isId) {
+		user = getTwitchUser(user.substring('id:'.length));
+	}
+	let timeoutreason = document.getElementById('timeoutreason').value.trim();
+	let textMessage = `/timeout ${user}`;
+	if (timeoutreason != null && timeoutreason != '') textMessage += ` ${timeoutreason}`;
+	await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: textMessage
+		})
+	}).then(res => {
+		if (!res.ok)
+			console.log(`Non-OK-Response (timeout): ${res.status}`);
+		return res.json();
+	}).then(data => {
+		document.getElementById('result').value = JSON.stringify(data, null, 2);
+	}).catch(err => {
+		console.log(`Error sending Twitch Message: ${err}`);
+	});
+}
+
+async function ban() {
+	let guid = document.getElementById('acc_id').value;
+	let jwtToken = document.getElementById('jwt_token').value;
+	let user = document.getElementById('banuser').value.toLowerCase();
+	let isId = user.startsWith('id:');
+	if (isId) {
+		user = getTwitchUser(user.substring('id:'.length));
+	}
+	let banreason = document.getElementById('banreason').value.trim();
+	let textMessage = `/ban ${user}`;
+	if (banreason != null && banreason != '') textMessage += ` ${banreason}`;
+	await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: textMessage
+		})
+	}).then(res => {
+		if (!res.ok)
+			console.log(`Non-OK-Response (ban): ${res.status}`);
+		return res.json();
+	}).then(data => {
+		document.getElementById('result').value = JSON.stringify(data, null, 2);
+	}).catch(err => {
+		console.log(`Error sending Twitch Message: ${err}`);
+	});
+}
+
+async function unban() {
+	let guid = document.getElementById('acc_id').value;
+	let jwtToken = document.getElementById('jwt_token').value;
+	let user = document.getElementById('unbanuser').value.toLowerCase();
+	let isId = user.startsWith('id:');
+	if (isId) {
+		user = getTwitchUser(user.substring('id:'.length));
+	}
+	let textMessage = `/unban ${user}`;
+	await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: textMessage
+		})
+	}).then(res => {
+		if (!res.ok)
+			console.log(`Non-OK-Response (unban): ${res.status}`);
+		return res.json();
+	}).then(data => {
+		document.getElementById('result').value = JSON.stringify(data, null, 2);
+	}).catch(err => {
+		console.log(`Error sending Twitch Message: ${err}`);
+	});
+}
+
+async function del() {
+	let guid = document.getElementById('acc_id').value;
+	let jwtToken = document.getElementById('jwt_token').value;
+	let messageid = document.getElementById('deleteid').value.trim();
+	let textMessage = `/delete ${messageid}`;
+	await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: textMessage
+		})
+	}).then(res => {
+		if (!res.ok)
+			console.log(`Non-OK-Response (delete): ${res.status}`);
+		return res.json();
+	}).then(data => {
+		document.getElementById('result').value = JSON.stringify(data, null, 2);
+	}).catch(err => {
+		console.log(`Error sending Twitch Message: ${err}`);
+	});
+}
+
+async function clear() {
+	let guid = document.getElementById('acc_id').value;
+	let jwtToken = document.getElementById('jwt_token').value;
+	let textMessage = '/clear';
+	await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json; charset=utf-8',
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			message: textMessage
+		})
+	}).then(res => {
+		if (!res.ok)
+			console.log(`Non-OK-Response (clear): ${res.status}`);
 		return res.json();
 	}).then(data => {
 		document.getElementById('result').value = JSON.stringify(data, null, 2);
